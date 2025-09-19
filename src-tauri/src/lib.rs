9398cc8edb9980d8ea538b219dee041ca7096b9a
heon <ffff00yllw@gmail.com>
2025-09-19 17:07:27 +0900

    fix: window state plugin 제거

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,6 @@ mod plugins;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
         .setup(setup::setup_app)
         .plugin(plugins::configure_sql())
         .plugin(plugins::configure_opener())
