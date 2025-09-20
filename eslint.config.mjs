2fe9f4e29141706953998a24d20d6c6f47bbde41
0viii0viii <ffff00yllw@gmail.com>
2025-09-20 10:31:51 +0900

    chore: eslint unused-imports plugin update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ import pluginImport from "eslint-plugin-import";
 import pluginReact from "eslint-plugin-react";
 import reactCompiler from "eslint-plugin-react-compiler";
 import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -27,6 +28,7 @@ export default [
       "react-compiler": reactCompiler,
       import: pluginImport,
       "simple-import-sort": pluginSimpleImportSort,
+      "unused-imports": unusedImports,
     },
     rules: {
       "react-compiler/react-compiler": "error",
@@ -35,9 +37,24 @@ export default [
       "import/first": "error",
       "import/newline-after-import": "error",
       "import/no-duplicates": "error",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "off", // unused-imports 플러그인에서 처리
+    },
+  },
   pluginJs.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
