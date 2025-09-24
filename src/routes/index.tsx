545ddbeed75e7170fbfa3c00612359f419d04333
heon <ffff00yllw@gmail.com>
2025-09-24 16:30:28 +0900

    fix: beforeLoad return redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({
   beforeLoad: async ({ context }) => {
     if (!context.auth.isAuthenticated) {
-      return redirect({ to: "/sign-in" });
+      throw redirect({ to: "/sign-in" });
     }
   },
   component: RouteComponent,
